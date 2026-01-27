92da95bda521dc15b2b0816e6bed14d985e9d7be
Angadveer Singh Chhoker <angad185singh@gmail.com>
2026-01-27 11:31:54 +0530

    [FRONTEND] | Completed Light Mode / Dark Mode. Added a Contributors component

--- apps/client/src/component/AllAdmins/v1/V1AllAdmins.tsx
+++ apps/client/src/component/AllAdmins/v1/V1AllAdmins.tsx
@@ -5,14 +5,20 @@ import SideBar from "@/component/general/SideBar";
 import React, { useEffect, useState } from "react";
 import DashboardInfo from "./DashboardInfo";
 import { Plus } from "lucide-react";
+<<<<<<< Updated upstream
 import AdminForm from "./AdminForm";
 import { createAdmin } from "@/api/admins/create-admin";
 import toast from "react-hot-toast";
+=======
+import VendorForm from "./VendorForm";
+import { useColors } from "@/component/general/(Color Manager)/useColors";
+>>>>>>> Stashed changes
 
 function V1AllAdmins() {
   const [data, setData] = useState([]);
   const [addNew, setAddNew] = useState(false);
   const [filteredData, setFilteredData] = useState([]);
+  const Colors = useColors();
   useEffect(() => {
     getAllAdmins(setData);
   }, []);
@@ -28,20 +34,25 @@ function V1AllAdmins() {
     }
   };
   return (
+<<<<<<< Updated upstream
     <div className="flex">
       {addNew && <AdminForm openForm={setAddNew} onSubmit={handleCreateAdmin} />}
+=======
+    <div className={`flex ${Colors.background.primary}`}>
+      {addNew && <VendorForm openForm={setAddNew} />}
+>>>>>>> Stashed changes
       <div className="h-screen">
         <SideBar />
       </div>
 
       <div className="ml-10 mt-10 w-full">
         <div className="w-[80%] mx-auto mb-5 flex justify-between">
-          <h1 className="text-3xl ml-3 text-white/60">Manage Admins</h1>
+          <h1 className={`text-3xl ml-3 ${Colors.text.secondary}`}>Manage Admins</h1>
           <button
             onClick={() => setAddNew(true)}
-            className="text-primaryBlue flex gap-2 border-primaryBlue border p-2 rounded-xl"
+            className={`${Colors.text.special} flex gap-2 border-primaryBlue border p-2 rounded-xl cursor-pointer ${Colors.hover.special} transition`}
           >
-            <Plus className="text-primaryBlue" />
+            <Plus className={`${Colors.text.special}`} />
             Add Admins
           </button>
         </div>
